6ac2a71df8b24a7f1544fc4a20d73019cec5dcd7
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-10-03 12:45:20 +0200

    iteration#3

---
+++ iteration3.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//ITERATION 3
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (const places of placesToTravel) {
+    console.log(places)
+}
